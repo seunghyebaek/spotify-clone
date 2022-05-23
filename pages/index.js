c64532d447a9710fcf657e2285712ec5db53622c
SH Baek <seunghyebaek0509@gmail.com>
2022-05-23 10:24:53 +0100

    main

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import { getSession, useSession } from "next-auth/react";
+import Center from "../components/Center";
+import Sidebar from "../components/Sidebar";
+
+export default function Home() {
+  const { data: session, status } = useSession();
+
+  console.log("CLIENT SESSION IS >>> ", session);
+
+  return (
+    <div className="bg-black h-screen overflow-hidden">
+      <main className="overflow-hidden scrollbar-hide flex">
+        <Sidebar />
+        <Center />
+      </main>
+
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  return {
+    props: { session },
+  };
+}
